47438aba55c2770dc2fe6b4558d4f49c6b7330a9
Joe-Bresee <jkbresee@pm.me>
2025-10-08 14:47:47 -0700

    lots of changes and database interactions

--- phone_number_normalizer/main.go
+++ phone_number_normalizer/main.go
@@ -1,6 +1,130 @@
 package main
 
-import "regexp"
+import (
+	"database/sql"
+	"fmt"
+	"regexp"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "jkbresee"
+	password = "password"
+	dbname   = "gophercises_phone"
+)
+
+type phone struct {
+	id     int
+	number string
+}
+
+func main() {
+	var ids []int
+	var numbers []string
+
+	phones := []string{
+		"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"123-456-7890",
+		"1234567892",
+		"(123)456-7892",
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=postgres sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+
+	err = resetDB(db, dbname)
+	must(err)
+
+	err = createDB(db, dbname)
+	must(err)
+
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+
+	defer db.Close()
+	err = db.Ping()
+	must(err)
+
+	err = createPhoneTable(db)
+	must(err)
+
+	for _, p := range phones {
+		id, err := insertPhone(db, normalize(p))
+		must(err)
+		ids = append(ids, id)
+	}
+	for i := range phones {
+		number, err := getPhone(db, ids[i])
+		must(err)
+		numbers = append(numbers, number)
+	}
+
+	for i := range phones {
+		fmt.Printf("ID: %d, Phone: %s\n", ids[i], numbers[i])
+	}
+}
+
+func getPhone(db *sql.DB, id int) (string, error) {
+	var number string
+	err := db.QueryRow("SELECT value FROM phone_numbers WHERE id=$1", id).Scan(&number)
+	if err != nil {
+		return "", err
+	}
+	return number, nil
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func createPhoneTable(db *sql.DB) error {
+	statement := `
+		CREATE TABLE IF NOT EXISTS phone_numbers (
+			id SERIAL,
+			value VARCHAR(255)
+			)`
+	_, err := db.Exec(statement)
+	return err
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name) //not production-grade sql query!!
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("[^0-9]")
